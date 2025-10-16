6c3f32ab5ae48ce434ff99d6c69ec2146ee7dd75
Luis Butron <butron0407@gmail.com>
2025-10-16 11:10:44 -0600

    Desarrollo de test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,17 @@
 public class Calculator {
 
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return (double) a / b;
+    }
 }
